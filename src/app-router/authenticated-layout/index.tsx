4798802b9931417cbf8ab33bd4adb9c5b36217cb
macminhtung <macminhtung1993@gmail.com>
2025-07-03 16:45:01 +0700

    update css

--- src/app-router/authenticated-layout/index.tsx
+++ src/app-router/authenticated-layout/index.tsx
@@ -28,7 +28,7 @@ const AuthenticatedLayout = () => {
 
   if (isAppLoading)
     return (
-      <div className='flex h-full w-full items-center justify-between'>
+      <div className='flex h-full w-full items-center justify-center'>
         <Loader className='scale-[6] animate-spin-2s' />
       </div>
     );
